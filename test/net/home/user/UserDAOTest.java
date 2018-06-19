6942655622f0d41ecb5ccb339f2b9ff859fd2e35
bok-PC\bok <bok_0120@naver.com>
2018-06-19 17:21:54 +0900

    개인 정보 수정 개발 완료

--- test/net/home/user/UserDAOTest.java
+++ test/net/home/user/UserDAOTest.java
@@ -30,6 +30,12 @@ public class UserDAOTest {
 		
 		User dbUser = userDao.findByUserId(user.getUserId());
 		assertEquals(user, dbUser);
+		
+		User updateUser = new User(user.getUserId(), "uPassword", "update_name", "update@slipp.net");
+		userDao.updateUser(updateUser);
+		
+		dbUser = userDao.findByUserId(updateUser.getUserId());
+		assertEquals(updateUser, dbUser);
 	}
 	
 	@Test
