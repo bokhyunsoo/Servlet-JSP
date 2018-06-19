92944532bcad7225d537c6bcd7961ac1433e4ad7
bok-PC\bok <bok_0120@naver.com>
2018-06-19 10:15:51 +0900

    Servlet 3.0에서 Annotation 활용한 매핑

--- src/net/home/HelloWorldServlet.java
+++ src/net/home/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(name="HelloWorld", urlPatterns={"/helloworld", "/hello", "/hello/world"})
 public class HelloWorldServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
